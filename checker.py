de083e9044714fdb20f0bd21f946807c434acfb0
fsgdfe <110311243+fsgdfe@users.noreply.github.com>
2022-07-31 16:01:51 +0300

    Update checker.py

--- checker.py
+++ checker.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat May 30 03:00:43 2020
+Created on Sat May 30 03:00:33 2020
 
 @author: ishan.m.jain
 """
@@ -11,22 +11,22 @@ import time
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-f = open("usernames.csv", "r")
+f = open("usernames3.txt", "r")
 data = f.readlines()
 
 
 
 for un in data:
-    time.sleep(4 * random.random())
-    url = "https://www.instagram.com/"+str(un[:4])
+    time.sleep(3 * random.random())
+    url = "https://www.tiktok.com/@"+str(un[:3])
     x = requests.get(url, verify = False)
-    if 'document-policy' in x.headers:
-        print("skip - " + un[:4]+ " - "+ str(data.index(un)))
-    else:
-        print("available - " + un[:4] + " - "+ str(data.index(un)))
+    if x.status_code == 404:
+        print("available - " + un[:3]+ " - " + str(x.status_code) +" "+ str(data.index(un)))
         f2 = open("available.txt", "a")
         f2.write(un)
         f2.close()
+    else:
+        print("skip - " + un[:3]+ " - " + str(x.status_code) +" "+ str(data.index(un)))
     if int(data.index(un))%20 == 0:
         time.sleep(15)
 
